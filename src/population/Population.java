7a61aa56fd1124bd4a35a003b694b1b9151c2107
Michal Nowakowski <michal951now@gmail.com>
2018-11-06 00:06:17 +0100

    added methods to getting seconds most fittest chromosome

--- src/population/Population.java
+++ src/population/Population.java
@@ -1,13 +1,12 @@
 package population;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class Population {
 	private final static int POPULATION_QUANTITY = 5;
-	private final static int GENES_QUANTITY = 6;
+	private final static int GENES_QUANTITY = 8;
 
 	private List<String> chromosomes = new ArrayList<String>();
 
@@ -33,53 +32,58 @@ public class Population {
 		}
 	}
 
-	private int getFitness(int index) {
+	private int getFitness(List<String> list, int index) {
 
 		int fitness = 0;
 
 		for (int i = 0; i < GENES_QUANTITY; i++) {
-			if (chromosomes.get(index).toCharArray()[i] == '1') {
+			if (list.get(index).toCharArray()[i] == '1') {
 				fitness++;
 			}
 		}
 
 		return fitness;
 	}
-	
-	private String getFittest() {
+
+	private String getFittest(List<String> list) {
 		int fittest = Integer.MIN_VALUE;
 		int fittestIndex = 0;
-		
-		for(int i = 0; i < POPULATION_QUANTITY; i++) {
-			if(fittest <= getFitness(i)) {
-				fittest = getFitness(i);
+
+		for (int i = 0; i < list.size(); i++) {
+			if (fittest <= getFitness(list, i)) {
+				fittest = getFitness(list, i);
 				fittestIndex = i;
 			}
 		}
-		
-		return chromosomes.get(fittestIndex);
+
+		return list.get(fittestIndex);
 	}
-	
-	private String getSecondFittest() {
-		
-		return null;
+
+	private String getSecondFittest(List<String> list) {
+
+		List<String> tmp = new ArrayList<String>(list);
+		String fittest = getFittest(tmp);
+		int removeIndex = 0;
+
+		for (int i = POPULATION_QUANTITY - 1; i >= 0; i--) {
+			if (tmp.get(i).equals(fittest)) {
+				removeIndex = i;
+				break;
+			}
+		}
+		tmp.remove(removeIndex);
+		return getFittest(tmp);
 	}
-	
-	public List<String> getTwoMostFittest(){
-		
-		int fittest = Integer.MIN_VALUE;
-		int fittestIndex = 0;
-		
+
+	public List<String> getTwoMostFittest() {
+
 		List<String> twoFittest = new ArrayList<String>();
-		List<String> tmp = new ArrayList<String>(chromosomes);
-		
-				
-		twoFittest.add(getFittest());	
-			
+
+		twoFittest.add(getFittest(chromosomes));
+		twoFittest.add(getSecondFittest(chromosomes));
+
 		return twoFittest;
-		
+
 	}
-	
-	
-	
+
 }
