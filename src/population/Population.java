fb5a81fa267a6230480a1e7631ebf5d21fb122c1
Michal Nowakowski <michal951now@gmail.com>
2018-11-06 20:57:01 +0100

    changed mutation() to private and added addTheFittestOffspring() implementation

--- src/population/Population.java
+++ src/population/Population.java
@@ -45,7 +45,7 @@ public class Population {
 		return fitness;
 	}
 
-	public String getFittest(List<String> list) {
+	private String getFittest(List<String> list) {
 		int fittest = Integer.MIN_VALUE;
 		int fittestIndex = 0;
 
@@ -57,6 +57,20 @@ public class Population {
 		}
 
 		return list.get(fittestIndex);
+	}
+
+	private int getLessFittest(List<String> list) {
+		int lessFittest = Integer.MAX_VALUE;
+		int lessFittestIndex = 0;
+
+		for (int i = 0; i < list.size(); i++) {
+			if (lessFittest >= getFitness(list, i)) {
+				lessFittest = getFitness(list, i);
+				lessFittestIndex = i;
+			}
+		}
+
+		return lessFittestIndex;
 	}
 
 	private String getSecondFittest(List<String> list) {
@@ -85,55 +99,74 @@ public class Population {
 		return twoFittest;
 
 	}
-	
-	public List<String> crossover(){
-		
+
+	public List<String> crossover() {
+
 		int crossoverPoint = random.nextInt(GENES_QUANTITY);
 		String fittest = selection().get(0);
 		String secondFittest = selection().get(1);
 		List<String> listAfterCrossover = new ArrayList<String>();
 		char[] fittestArr = fittest.toCharArray();
 		char[] secondFittestArr = secondFittest.toCharArray();
-		
-		for(int i = 0 ; i < crossoverPoint ; i++) {
+
+		for (int i = 0; i < crossoverPoint; i++) {
 			char tmp = fittestArr[i];
 			fittestArr[i] = secondFittestArr[i];
-			
+
 			secondFittestArr[i] = tmp;
-			
+
 		}
-		
-		
-		
+
 		listAfterCrossover.add(String.valueOf(fittestArr));
 		listAfterCrossover.add(String.valueOf(secondFittestArr));
-		
+
 		return listAfterCrossover;
 	}
-	
-	public List<String> mutation(){
-		
+
+	private List<String> mutation() {
+
 		int mutationPoint = random.nextInt(GENES_QUANTITY);
 		char[] fittestAfterCrossover = crossover().get(0).toCharArray();
 		char[] secondFittestAfterCrossover = crossover().get(1).toCharArray();
 		List<String> list = new ArrayList<String>();
-		
-		if(fittestAfterCrossover[mutationPoint] == '0') {
+
+		if (fittestAfterCrossover[mutationPoint] == '0') {
 			fittestAfterCrossover[mutationPoint] = '1';
-		}else {
+		} else {
 			fittestAfterCrossover[mutationPoint] = '0';
 		}
-		
+
 		mutationPoint = random.nextInt(GENES_QUANTITY);
-		if(secondFittestAfterCrossover[mutationPoint] == '0') {
+		if (secondFittestAfterCrossover[mutationPoint] == '0') {
 			secondFittestAfterCrossover[mutationPoint] = '1';
-		}else {
+		} else {
 			secondFittestAfterCrossover[mutationPoint] = '0';
 		}
-		
+
 		list.add(String.valueOf(fittestAfterCrossover));
 		list.add(String.valueOf(secondFittestAfterCrossover));
 		return list;
+	}
+
+	public void addTheFittestOffspring() {
+
+		List<String> mutationList = mutation();
+
+		System.out.println("Now executing mutation result, below \n" + mutationList);
+
+		List<String> fittest = new ArrayList<String>();
+		fittest.add((mutationList.get(0)));
+		List<String> secondFittest = new ArrayList<String>();
+		secondFittest.add((mutationList.get(1)));
+
+		if (getFitness(fittest, 0) > getFitness(secondFittest, 0)) {
+			chromosomes.remove(getLessFittest(chromosomes));
+			chromosomes.add(fittest.get(0));
+		} else {
+			chromosomes.remove(getLessFittest(chromosomes));
+			chromosomes.add(secondFittest.get(0));
+		}
+
 	}
 
 }
