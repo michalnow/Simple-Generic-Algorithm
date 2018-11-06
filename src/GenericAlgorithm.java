f60cd6e87dd8cc495a7a2f55db9ee7ceaba5c98a
Michal Nowakowski <michal951now@gmail.com>
2018-11-06 21:20:03 +0100

    added while loop to determinate in which generation we will have a solution

--- src/GenericAlgorithm.java
+++ src/GenericAlgorithm.java
@@ -1,17 +1,25 @@
 import population.Population;
 
 public class GenericAlgorithm {
-	
-	
+
 	public static void main(String[] args) {
+		int generation = 0;
 		Population population = new Population();
 		population.randomizePopulation();
+		System.out.println("Generation nr " + generation + " ||| fitness level = " + population.getMaxFitnessLevel());
+		while (population.getMaxFitnessLevel() < population.GENES_QUANTITY) {
+			++generation;
+			System.out.println("Now Executing selection, result below \n" + population.selection());
+			System.out.println("Now execeuting crossover, result below \n" + population.crossover());
+			population.addTheFittestOffspring();
+			System.out
+					.println("Generation nr " + generation + " ||| fitness level = " + population.getMaxFitnessLevel());
+		}
+
+		System.out.println("\n\n\nSolution find in generation number = " + generation + "\nFitness level = "
+				+ population.getMaxFitnessLevel());
 		population.showPopulation();
-		System.out.println("Now Executing selection, result below \n" + population.selection());
-		System.out.println("Now execeuting crossover, result below \n" + population.crossover());
-		population.addTheFittestOffspring();
-		population.showPopulation();
-		
+
 	}
-	
+
 }
