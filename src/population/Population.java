236a9534901ea4a8d1cdacafcb08119554b6fb61
Michal Nowakowski <michal951now@gmail.com>
2018-11-06 21:50:54 +0100

    code formatting

--- src/population/Population.java
+++ src/population/Population.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 public class Population {
 	public final static int POPULATION_QUANTITY = 5;
-	public final static int GENES_QUANTITY = 8;
+	public final static int GENES_QUANTITY = 1000;
 
 	private List<String> chromosomes = new ArrayList<String>();
 
