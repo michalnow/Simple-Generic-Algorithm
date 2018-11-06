cd4e32fea847534bbbb41103719378860c97e37a
Michal Nowakowski <michal951now@gmail.com>
2018-11-06 19:27:11 +0100

    added new method mutation()

--- src/GenericAlgorithm.java
+++ src/GenericAlgorithm.java
@@ -9,7 +9,10 @@ public class GenericAlgorithm {
 		population.showPopulation();
 		System.out.println("===============");
 		System.out.println(population.selection());
+		System.out.println("!!!!!!!!!!!!");
 		System.out.println(population.crossover());
+		System.out.println("!!!!!!!!!!!!");
+		System.out.println(population.mutation());
 	}
 	
 }
