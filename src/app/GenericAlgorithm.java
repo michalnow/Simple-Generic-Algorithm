a68962b2321727d04f6d545baa5e40aa53f208c5
Michal Nowakowski <michal951now@gmail.com>
2018-11-22 19:38:35 +0100

    added switch to decide whether program should be executing by one thread or more

--- src/app/GenericAlgorithm.java
+++ src/app/GenericAlgorithm.java
@@ -1,9 +1,11 @@
 package app;
+import java.util.Scanner;
+
 import population.Population;
 
 public class GenericAlgorithm {
 
-	public static void main(String[] args) {
+	private static void singleThreadExecution() {
 		int generation = 0;
 		Population population = new Population();
 		population.randomizePopulation();
@@ -11,10 +13,6 @@ public class GenericAlgorithm {
 		System.out.println("Generation nr " + generation + " ||| fitness level = " + population.getMaxFitnessLevel());
 		while (population.getMaxFitnessLevel() < population.GENES_QUANTITY) {
 			++generation;
-			population.selection();
-			population.crossover();
-			//System.out.println("Now Executing selection, result below \n" + population.selection());
-			//System.out.println("Now execeuting crossover, result below \n" + population.crossover());
 			population.addTheFittestOffspring();
 			System.out
 					.println("Generation nr " + generation + " ||| fitness level = " + population.getMaxFitnessLevel());
@@ -24,6 +22,35 @@ public class GenericAlgorithm {
 				+ population.getMaxFitnessLevel());
 		population.showPopulation();
 
-		System.out.println("It Took " + (endTime - startTime) / 1000 + " seconds");
+		System.out.println("It Took " + (endTime - startTime)  + " ms");
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String choice ="";
+		
+		System.out.println("Time to decide:\n1 - Single thread execution\n2 - Multi thread execution1");
+		choice = scanner.next("[12]");
+		
+		switch(choice) {
+		
+			case "1" : {
+				singleThreadExecution();
+				break;
+			}
+			
+			case "2" : {
+				System.out.println("Multi Thread");
+				break; 
+			}
+			
+			default: {
+				System.out.println("Wrong input");
+			}
+		}
+		
+		scanner.close();
+		
 	}
 }
