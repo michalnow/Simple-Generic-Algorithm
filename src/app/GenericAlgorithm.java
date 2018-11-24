0082ef359dbf349ded3b5f9a0302fd1e7f9d7e81
Michał Nowakowski <michal951@poczta.onet.pl>
2018-11-24 16:48:07 +0100

    added possibility to choose number of threads

--- src/app/GenericAlgorithm.java
+++ src/app/GenericAlgorithm.java
@@ -2,6 +2,7 @@ package app;
 import java.util.Scanner;
 
 import population.Population;
+import threading.PopulationThread;
 
 public class GenericAlgorithm {
 
@@ -13,7 +14,7 @@ public class GenericAlgorithm {
 		long startTime = System.currentTimeMillis();
 		System.out.println("Generation nr " + generation + " ||| fitness level = " + population.getMaxFitnessLevel());
 		
-		while (population.getMaxFitnessLevel() < population.GENES_QUANTITY) {
+		while (population.getMaxFitnessLevel() < Population.GENES_QUANTITY) {
 			++generation;
 			population.addTheFittestOffspring();
 			System.out
@@ -26,6 +27,7 @@ public class GenericAlgorithm {
 		population.showPopulation();
 
 		System.out.println("It Took " + (endTime - startTime)  + " ms");
+		System.out.println(population.getChromosomes());
 	}
 	
 	public static void main(String[] args) {
@@ -33,7 +35,7 @@ public class GenericAlgorithm {
 		
 		String choice ="";
 		
-		System.out.println("Time to decide:\n1 - Single thread execution\n2 - Multi thread execution1");
+		System.out.println("Time to decide:\n1 - Single thread execution\n2 - Multi thread execution");
 		choice = scanner.next("[12]");
 		
 		switch(choice) {
@@ -44,7 +46,21 @@ public class GenericAlgorithm {
 			}
 			
 			case "2" : {
+				System.out.println("Number of threads: ");
+				int noThreads = scanner.nextInt();
+				long startTime = System.currentTimeMillis();
+
+				Population population = new Population();
+				population.randomizePopulation();
 				System.out.println("Multi Thread");
+				for(int i = 0 ; i< noThreads ; i++) {
+					Thread thread = new Thread(new PopulationThread(population,noThreads));
+					thread.start();
+				}
+				
+				long endTime = System.currentTimeMillis();
+				System.out.println("It Took " + (endTime - startTime)  + " ms");
+
 				break; 
 			}
 			
