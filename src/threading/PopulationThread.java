218fd63c37be2e2263e5884d54cf25b25c1f71c9
Michał Nowakowski <michal951@poczta.onet.pl>
2018-11-24 16:49:44 +0100

    works for one thread

--- src/threading/PopulationThread.java
+++ src/threading/PopulationThread.java
@@ -1,21 +1,58 @@
 package threading;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 import population.Population;
 
 public class PopulationThread implements Runnable {
-	
-	private Population population;
-	
+
+	private static Population population;
+	private int noThreads;
+	private List<String> chromos;
+
 	public PopulationThread(Population population, int noThreads) {
-		this.population = population;
+
+		PopulationThread.population = population;
+		this.noThreads = noThreads;
+		chromos = new ArrayList<String>();
+
 	}
-	
+
+	private List<String> divideChromosomes() {
+		List<String> list = new ArrayList<String>();
+		List<String> chromosomes = population.getChromosomes();
+		System.out.println(chromosomes + "\n");
+		String a = "";
+		int endCut = Population.GENES_QUANTITY / noThreads;
+		int chromosomeLength = chromosomes.get(0).length();
+		
+		for (int i = 0; i < chromosomes.size(); i++) {
+			a = chromosomes.get(i).substring(0, endCut);
+			list.add(a);
+		}
+		
+		List<String> newList = new ArrayList<String>();
+		for (int i = 0; i < chromosomes.size(); i++) {
+			a = chromosomes.get(i).substring(endCut, chromosomeLength-1);
+			newList.add(a);
+		}
+		
+
+		return list;
+	}
+
 	@Override
 	public void run() {
-		
-		
+		population.setChromosomes(divideChromosomes());
+		population.showPopulation();
+
+		while (population.getMaxFitnessLevel() < Population.GENES_QUANTITY / noThreads) {
+			population.addTheFittestOffspring();
+
+		}
+		System.out.println(population.getMaxFitnessLevel());
+
 	}
 
 }
